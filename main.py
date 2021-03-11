531e61e0854c80b9f032b3c5f080833065be1898
Irwin Dominguez <irwind500@gmail.com>
2021-03-10 20:37:01 -0600

    imported libraries

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-#dev branch
-print("Hello World")
-print("Start")
+'''
+Program: Recipe Web Scrpaper
+Description: A Python script that ingests a given website and turn back the desired Recipe Ingredients and Directions.
+Purpose: Personal Project
+Author: Irwin Dominguez
+'''
+#import necessary modules
+import requests
+import bs4
+
+print("hello")
+
+
+website_url = input("What recipe would you like to extract? ")
+print("goddby")
+print(website_url)
